5c7ac25ebf506581b11c1aff8a6531615344635d
Zeyad Mohammed Slama <93527235+Demo-23home@users.noreply.github.com>
2023-03-09 03:31:08 +0200

    add_Navbar,initial_Customization

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts=[
+    {
+    'author': 'Demo',
+    'title': 'Blog Post1',
+    'content':'First Post Content',
+    'date_posted':'Augest 26,2018'
+    },
+     {
+    'author': 'Zeyad',
+    'title': 'Blog Post2',
+    'content':'Second Post Content',
+    'date_posted':'October 3,2019'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context={
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h2>Blog About</h2>')
+    return render(request, 'blog/about.html',{'title':'About'})
