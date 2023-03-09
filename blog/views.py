c21e0557ef8e82cb738fb87f0eac1cf8de263d8a
Zeyad Mohammed Slama <93527235+Demo-23home@users.noreply.github.com>
2023-03-09 14:05:16 +0200

    adding_init_data

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts=[
-    {
-    'author': 'Demo',
-    'title': 'Blog Post1',
-    'content':'First Post Content',
-    'date_posted':'Augest 26,2018'
-    },
-     {
-    'author': 'Zeyad',
-    'title': 'Blog Post2',
-    'content':'Second Post Content',
-    'date_posted':'October 3,2019'
-    }
-]
+# posts=[
+#     {
+#     'author': 'Demo',
+#     'title': 'Blog Post1',
+#     'content':'First Post Content',
+#     'date_posted':'Augest 26,2018'
+#     },
+#      {
+#     'author': 'Zeyad',
+#     'title': 'Blog Post2',
+#     'content':'Second Post Content',
+#     'date_posted':'October 3,2019'
+#     }
+# ]
 
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
