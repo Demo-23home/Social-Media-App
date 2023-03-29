f02b9897933f9b8aa789330798d5e1790581b998
Zeyad Mohammed Slama <93527235+Demo-23home@users.noreply.github.com>
2023-03-29 23:13:48 +0200

    adding login&logout&profile

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views as users_views
 
 urlpatterns = [
-    path('',users_views.register)
+    path('',users_views.register,name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 # from django.contrib.auth.forms import UserCreationForm  
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,11 +11,16 @@ def register(request):
         form =UserRegisterForm(request.POST)
         if form.is_valid():
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account Created for {username}!')
+            messages.success(request,f'your account has been created you can now login!')
             form.save()
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form =UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
 
 
+
+@login_required
+def profile(request):
+
+    return render(request,'users/profile.html')
