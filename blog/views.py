d410d4a9fc19f80fadcbc586bf9cf1f776ea655e
Zeyad Mohammed Slama <93527235+Demo-23home@users.noreply.github.com>
2023-04-18 00:28:25 +0200

    add creating_post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Post
+from django.urls import reverse
 # Create your views here.
-
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView
+                                  )
 # posts=[
 #     {
 #     'author': 'Demo',
@@ -25,5 +29,28 @@ def home(request):
     }
     return render(request, 'blog/home.html',context)
 
+class PostListView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']   #>>'date_posted'>>oldest to newest , add '-' to reverse
+
+
+
 def about(request):
     return render(request, 'blog/about.html',{'title':'About'})
+
+
+class PostDetailView(DetailView):
+    model=Post
+
+
+class PostCreateView(CreateView):
+    model=Post
+    fields=['title','content']
+        
+
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
