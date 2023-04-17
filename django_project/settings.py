cb31ef459e197bb326fba758e242203f4593ebd2
Zeyad Mohammed Slama <93527235+Demo-23home@users.noreply.github.com>
2023-04-17 17:33:02 +0200

    adding_user_profile_pic,creating_profileWithUser

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,10 +128,18 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
 
+STATIC_URL='/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL ='login'
+
+
+
+
