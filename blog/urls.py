a9b52bdd8adfd692b82dd573de1320476d8c6fda
Zeyad Mohammed Slama <93527235+Demo-23home@users.noreply.github.com>
2023-04-18 05:32:09 +0200

    adding create_post,delete_post

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .import views
 from .views import (PostListView,
                     PostDetailView,
-                    PostCreateView
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView
                     )
 
 urlpatterns = [
@@ -10,5 +12,9 @@ path('',PostListView.as_view(),name='blog-home'),  #blog/post_list.html  >> <app
 path('about/',views.about,name='blog-about'),
 path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
 path('post/new/',PostCreateView.as_view(),name='post-create'),
+path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
+path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
+
+
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Post
-from django.urls import reverse
+# from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView
                                   )
 # posts=[
 #     {
@@ -31,26 +34,53 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name='blog/home.html'
-    context_object_name='posts'
-    ordering=['-date_posted']   #>>'date_posted'>>oldest to newest , add '-' to reverse
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']   #>>'date_posted'>>oldest to newest , add '-' to reverse
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+ 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+        
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+        
 
 
 
 def about(request):
     return render(request, 'blog/about.html',{'title':'About'})
-
-
-class PostDetailView(DetailView):
-    model=Post
-
-
-class PostCreateView(CreateView):
-    model=Post
-    fields=['title','content']
-        
-
-
-    def form_valid(self,form):
-        form.instance.author=self.request.user
-        return super().form_valid(form)
